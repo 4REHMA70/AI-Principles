e259f71f5baf1ce2cdda8ee1051a83506059c4c6
4REHMA70 <arehman3308@gmail.com>
2023-12-01 09:16:35 +0000

    Added spacing function!

--- maze.py
+++ maze.py
@@ -3,9 +3,10 @@ import numpy as np
 import math
 #
 class Maze:
-    def __init__(self, rows, cols, seed=None):
+    def __init__(self, rows, cols, space=None, seed=None):
         self.rows = rows
         self.cols = cols
+        self.space = space
         self.seed = seed
         self.matrix = [[1] * cols for _ in range(rows)]  # Initialize with walls. For each col in row, turn it into 1.
 
@@ -31,7 +32,7 @@ class Maze:
 
                 # Introduce randomness to cut across walls
                 random_neighbors = self.get_unvisited_neighbors(x, y)
-                if random_neighbors and random.random() < 0.45:  # Adjust the probability as needed
+                if random_neighbors and random.random() < 0.7:  # Adjust the probability as needed
                     random_neighbor = random.choice(random_neighbors)
                     rx, ry = random_neighbor
                     self.matrix[(x + rx) // 2][(y + ry) // 2] = 0  # Set a random cell between current and random neighbor as path
@@ -39,9 +40,17 @@ class Maze:
                 stack.append(next_cell)
             else:
                 stack.pop()  # Backtrack
+        self.spacing()
         return np.array(self.matrix)
 
+    def spacing(self):
+        if self.space:
+            for i in range(self.rows):
+                for j in range(self.space, self.cols - 1, self.space + 1):
+                    if self.matrix[i][j] == 0:
+                        self.matrix[i][j - 1] = 0  
 
+    
     def get_unvisited_neighbors(self, x, y): 
         neighbors = [(x + dx, y + dy) for dx, dy in [(2, 0), (-2, 0), (0, 2), (0, -2)]] # Defining potential neighbors, each 2 steps away, right, left, up, down. Adding dx and dy to x and y
         neighbors = [(nx, ny) for nx, ny in neighbors if 0 < nx < self.rows - 1 and 0 < ny < self.cols - 1 and self.matrix[nx][ny]] # For valid neighbors, get coords
@@ -80,24 +89,22 @@ class Maze:
                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
         self.matrix=maze
 
-    def set_start_and_goal(self):
+    def set_start_and_goal(self, min_distance=5):
             free_spaces = [(i, j) for i in range(self.rows) for j in range(self.cols) if self.matrix[i][j] == 0]
 
             if not free_spaces:
                 raise ValueError("No free spaces in the maze.")
-
-
+            
             while True:
                 start, goal = random.sample(free_spaces, 2)
-                min_distance=5
-                if math.dist(start,goal) >= min_distance:
+                if math.dist(start,goal) >= min_distance: # If euclidean dist greater than or equal to min_distance. Else repeat random start,goal instantiation
                     return start, goal
 
-# Example Usage
 if __name__ == "__main__":
-    maze = Maze(rows=9, cols=9, seed=42) #ROW, COL, AND SEED SPECIFIED HERE
+    maze = Maze(rows=6, cols=6, space=2, seed=42) # Row, Col, and Seed specified here!
     maze.generate_maze()
-    #maze.print_maze()
-    maze.predefined_maze()
+    maze.spacing()
+    maze.print_maze()
+    #maze.predefined_maze()
     maze.set_start_and_goal()
     #maze.print_maze()
